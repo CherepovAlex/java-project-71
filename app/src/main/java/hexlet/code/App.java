e7a16f6d57f6fe9370767697b57ecc6de24aa386
CherepovAlex <westler123@yandex.ru>
2024-11-30 11:17:40 +0300

    add picocli new options

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,17 +7,17 @@ import java.util.concurrent.Callable;
 
 @Command(name = "gendiff ", mixinStandardHelpOptions = true, description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
-    //    @CommandLine.Parameters(index = "0",  paramLabel = "filepath1", description = "path to first file")
+    @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
     private String filepath1;
-    //    @CommandLine.Parameters(index = "1",  paramLabel = "filepath2", description = "path to second file")
+    @CommandLine.Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
     private String filepath2;
 
-    //    @CommandLine.Option(
-//            names = {"--f", "--format"},
-//            paramLabel = "format",
-//            defaultValue = "stylish",
-//            description = "output format [default: stylish]"
-//    )
+    @CommandLine.Option(
+            names = {"--f", "--format"},
+            paramLabel = "format",
+            defaultValue = "stylish",
+            description = "output format [default: stylish]"
+    )
     private String format;
 
     @Override
