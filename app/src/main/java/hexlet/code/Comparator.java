b4368e68804328db8628eaed53b56665e6eb0a92
CherepovAlex <westler123@yandex.ru>
2024-12-01 11:18:17 +0300

    fix compare

--- app/src/main/java/hexlet/code/Comparator.java
+++ app/src/main/java/hexlet/code/Comparator.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
-import java.util.HashMap;
 
 public class Comparator {
 
@@ -16,7 +15,8 @@ public class Comparator {
         for (var key : keys) {
             boolean fileMap1In = fileMap1.containsKey(key);
             boolean fileMap2In = fileMap2.containsKey(key);
-            boolean file12Equals = fileMap1.get(key) != null && fileMap1.get(key) != null && ((fileMap1.get(key)).equals(fileMap2.get(key)));
+            boolean file12Equals = fileMap1.get(key) != null && fileMap1.get(key) != null
+                    && ((fileMap1.get(key)).equals(fileMap2.get(key)));
             if (fileMap1In && fileMap2In) {
                 if (file12Equals) {
                     list.add(Map.of("  " + (String) key, fileMap1.get(key)));
