3568329ca9e15556321978881d08625b73a9b982
CherepovAlex <westler123@yandex.ru>
2024-12-01 13:57:00 +0300

    add checkstyle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,12 @@ repositories {
 dependencies {
     implementation("info.picocli:picocli:4.7.6")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
