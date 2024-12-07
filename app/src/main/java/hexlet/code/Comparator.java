e6ecf27bd8b3a18d1f39aeec72381290a38544dc
CherepovAlex <westler123@yandex.ru>
2024-12-07 20:31:54 +0300

    fix Comparator for cc

--- app/src/main/java/hexlet/code/Comparator.java
+++ app/src/main/java/hexlet/code/Comparator.java
@@ -1,44 +1,37 @@
 package hexlet.code;
 
-import java.util.Map;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.TreeSet;
 
 public class Comparator {
 
-    public static List<Map<String, Object>> compare(Map<String, Object> fileMapFirst,
-                                                    Map<String, Object> fileMapSecond) {
+    public static List<Map<String, Object>> compare(Map<String, Object> fileMap1, Map<String, Object> fileMap2) {
         var listOfMaps = new ArrayList<Map<String, Object>>();
-        var keysOfAllMaps = new TreeSet<>();
-        keysOfAllMaps.addAll(fileMapFirst.keySet());
-        keysOfAllMaps.addAll(fileMapSecond.keySet());
-        for (var keyForCompare : keysOfAllMaps) {
-            var mapWithOrder = new HashMap<String, Object>();
-            Boolean fileMapFirstInclude = fileMapFirst.containsKey(keyForCompare);
-            Boolean fileMapSecondInlude = fileMapSecond.containsKey(keyForCompare);
-            Boolean fileMapFistSecobdEquals = null;
-            if (fileMapFirstInclude && fileMapSecondInlude) {
-                fileMapFistSecobdEquals = (fileMapFirst.get(keyForCompare)).equals(fileMapSecond.get(keyForCompare));
+        var setOfAllKeys = new TreeSet<>();
+        setOfAllKeys.addAll(fileMap1.keySet()); setOfAllKeys.addAll(fileMap2.keySet());
+        for (var key : setOfAllKeys) {
+            var map = new HashMap<String, Object>();
+            Boolean fileMap1Include = fileMap1.containsKey(key);
+            Boolean fileMap2Include = fileMap2.containsKey(key);
+            Boolean file12Equals = fileMap1.containsKey(key) && fileMap2.containsKey(key)
+                                    ? (fileMap1.get(key)).equals(fileMap2.get(key)) : null;
+            if (fileMap1Include && fileMap2Include) {
+                if (file12Equals) {
+                    map.put("  " + (String) key, fileMap1.get(key));
+                } else {
+                    map.put("- " + (String) key, fileMap1.get(key));
+                    map.put("+ " + (String) key, fileMap2.get(key));
+                }
             }
-            if (fileMapFirstInclude && fileMapSecondInlude && fileMapFistSecobdEquals) {
-                mapWithOrder.put("  " + (String) keyForCompare, fileMapFirst.get(keyForCompare));
-                listOfMaps.add(mapWithOrder);
+            if (fileMap1Include && !fileMap2Include) {
+                map.put("- " + (String) key, fileMap1.get(key));
+            } else if (!fileMap1Include && fileMap2Include) {
+                map.put("+ " + (String) key, fileMap2.get(key));
             }
-            if (fileMapFirstInclude && fileMapSecondInlude && !fileMapFistSecobdEquals) {
-                mapWithOrder.put("- " + (String) keyForCompare, fileMapFirst.get(keyForCompare));
-                mapWithOrder.put("+ " + (String) keyForCompare, fileMapSecond.get(keyForCompare));
-                listOfMaps.add(mapWithOrder);
-            }
-            if (fileMapFirstInclude && !fileMapSecondInlude) {
-                mapWithOrder.put("- " + (String) keyForCompare, fileMapFirst.get(keyForCompare));
-                listOfMaps.add(mapWithOrder);
-            }
-            if (!fileMapFirstInclude && fileMapSecondInlude) {
-                mapWithOrder.put("+ " + (String) keyForCompare, fileMapSecond.get(keyForCompare));
-                listOfMaps.add(mapWithOrder);
-            }
+            listOfMaps.add(map);
         }
         return listOfMaps;
     }
