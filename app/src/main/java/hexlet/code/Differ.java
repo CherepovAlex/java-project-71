ab3658f5d3d7ee97da8ccb4fc96176f4e79b176d
CherepovAlex <westler123@yandex.ru>
2025-01-18 13:28:28 +0300

    fix short methods

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -14,34 +14,25 @@ public class Differ {
     }
 
     public static String generate(String filepath1, String filepath2, String formatName) throws Exception {
-        Path fullFilePath1 = getFullPath(filepath1);
-        Path fullFilePath2 = getFullPath(filepath2);
+        String contentFile1 = Files.readString(getFullPath(filepath1));
+        String contentFile2 = Files.readString(getFullPath(filepath2));
 
-        String contentFile1 = readFileContent(fullFilePath1);
-        String contentFile2 = readFileContent(fullFilePath2);
+        String format1 = getDataFormat(filepath1);
+        String format2 = getDataFormat(filepath2);
 
-        Map<String, Object> fileMap1 = parseContent(contentFile1, getDataFormat(fullFilePath1.toString()));
-        Map<String, Object> fileMap2 = parseContent(contentFile2, getDataFormat(fullFilePath2.toString()));
+        Map<String, Object> fileMap1 = Parser.parseContent(contentFile1, format1);
+        Map<String, Object> fileMap2 = Parser.parseContent(contentFile2, format2);
 
         List<KeyStatus> diff = FileComparator.compare(fileMap1, fileMap2);
 
         return Formatter.choiceFormat(diff, formatName);
     }
 
-    private static String readFileContent(Path filePath) throws Exception {
-        return Files.readString(filePath);
-    }
-
-    private static Map<String, Object> parseContent(String content, String format) throws Exception {
-        return Parser.parseContent(content, format);
+    private static Path getFullPath(String path) {
+        return Paths.get(path).toAbsolutePath().normalize();
     }
 
     private static String getDataFormat(String filePath) {
-        int index = filePath.lastIndexOf('.');
-        return index > 0 ? filePath.substring(index + 1) : "";
-    }
-
-    public static Path getFullPath(String path) {
-        return Paths.get(path).toAbsolutePath().normalize();
+        return filePath.substring(filePath.lastIndexOf('.') + 1);
     }
 }
